0f29815e48026b901b116cf47b2d1eafb1a3925f
tw-chenyu <yuchen@thoughtworks.com>
2016-08-12 09:21:03 +0800

    [CY] - add house regiter class that user can rent house.

---
+++ test/com/thoughtworks/ootown/Model/RegisterTest.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.ootown.Model;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by yuchen on 8/11/16.
+ */
+public class RegisterTest {
+
+        Register register = null;
+
+        @org.junit.Before
+        public void setUp() throws Exception {
+            register = new Register();
+        }
+
+        @org.junit.Test
+        public void getHouseNum() throws Exception {
+            String person ="chenyu";
+            boolean new_person = register.register(person);
+            boolean exist_person = register.register(person);
+            boolean success = register.record.containsKey(person);
+            assertEquals(success, true);
+            assertEquals(new_person, true);
+            assertEquals(exist_person, false);
+
+        }
+
+}
